7faf62a849f13be3dec47bb17744c57413db8cbb
Saragc0 <sara.carolyne06@gmail.com>
2025-11-17 09:49:20 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
         </section>
         <section className="destaque">
 
-          <iframe src="https://www.youtube.com/embed/3VKnbGeC2IE?autoplay=1&mute=1&controls=0&loop=1&playlist=3VKnbGeC2IE&modestbranding=1&showinfo=0" frameborder="0" allow="autoplay; fullscreen"></iframe>
+          <iframe src="https://www.youtube.com/embed/qguG-6NAuGo?autoplay=1&mute=1&controls=0&loop=1&playlist=qguG-6NAuGo&modestbranding=1&showinfo=0" frameborder="0" allow="autoplay; fullscreen"></iframe>
 
         </section>
         
